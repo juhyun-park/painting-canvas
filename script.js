24a0b8e6c3e670d9a9d5beed224418fff9d0dc38
juhyun-park <juhyun.park1201@gmail.com>
2021-03-19 00:25:08 +0900

    javascript

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-let a=2;
+const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function startPainting() {
+    painting = true;
+}
+
+function stopPainting() {
+    painting = false;
+}
+
+function onMouseMove(e) {
+    const x = e.offsetX;
+    const y = e.offsetY;
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+}
+
+function onMouseDown(e) {
+    painting = true;
+}
+
+function onMouseUp(e) {
+    painting =false;
+}
+
+if(canvas) {
+    canvas.addEventListener('mousemove', onMouseMove);
+    canvas.addEventListener('mousedown', startPainting);
+    canvas.addEventListener('mouseup', stopPainting);
+    canvas.addEventListener('mouseleave', stopPainting);
+}
